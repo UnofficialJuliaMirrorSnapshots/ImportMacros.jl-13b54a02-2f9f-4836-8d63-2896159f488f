abf7412db5917533b2335002e871653a58797a41
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-05-01 01:04:06 +0000

    Snapshot of branch master taken on 2019-04-30T21:04:05.211-04:00 from "https://github.com/UnofficialJuliaMirror/ImportMacros.jl-13b54a02-2f9f-4836-8d63-2896159f488f"

--- src/ImportMacros.jl
+++ src/ImportMacros.jl
@@ -1,57 +1,49 @@
-__precompile__()
-
 module ImportMacros
-
-using MacroTools: @capture
 
 export @import, @using, @from
 
 
 """
-```julia
-@import MyLongModuleName as m
-```
+    @import LongModuleName as alias
 
-Load the module `MyLongModuleName` with `import` and binds it to `m`.
-Equivalent to
+Load the module `LongModuleName` with `import` and binds it to `alias`.
+Roughly equivalent to
 
 ```julia
-import MyLongModuleName
-const m = MyLongModuleName
+baremodule \$(gensym())
+    import LongModuleName
+end
+const alias = \$(gensym()).LongModuleName
 ```
 """
 :(@import)
 
 """
-```julia
-@using MyLongModuleName as m
-```
+    @using LongModuleName as alias
 
-Load the module `MyLongModuleName` with `using` and binds it to `m`.
-Equivalent to
+Load the module `LongModuleName` with `using` and binds it to `alias`.
+Roughly equivalent to
 
 ```julia
-using MyLongModuleName
-const m = MyLongModuleName
+using LongModuleName
+const alias = LongModuleName
 ```
 """
 :(@using)
 
 """
-```julia
-@from Module use object as alias
-@from Module use @object as @alias
-```
+    @from Module use object as alias
+    @from Module use @object as @alias
 
 Load the module `Module` with `import` and bind `object` to `alias`,
 or `@object` to `@alias` in the case of macros.
-
-Equivalent to:
+Roughly equivalent to:
 
 ```julia
-import Module
-const alias = Module.object
-@eval const \$(Symbol("@alias")) = Module.\$(Symbol("@object"))    # macros
+baremodule \$(gensym())
+    import Module
+end
+const alias = \$(gensym()).Module.object
 ```
 """
 :(@from)
@@ -59,68 +51,66 @@ const alias = Module.object
 # generate the macros
 for macroname in ("import", "using")
     @eval begin
-        macro $(Symbol(macroname))(M::Union{Symbol, Expr}, kw::Symbol, m::Symbol)
+        macro $(Symbol(macroname))(M::Union{Symbol, Expr}, kw::Symbol, alias::Symbol)
             # input check
-            kw == :as || throw(ArgumentError("syntax error: expected `as`, got `$kw`"))
-            isdefined(m) && throw(ArgumentError("alias `$m` already defined"))
-
-            # create operator expression
-            names = get_names(M)
-            ex = Expr(Symbol($macroname))
-            for n in names
-                push!(ex.args, n)
+            if kw !== :as
+                throw(ArgumentError("syntax error: expected `@$($macroname) Module as mod`"))
             end
-
-            return quote
-                $(esc(ex))
-                const $(esc(m)) = $(esc(M))
-                nothing
-            end
+            return create_expression(Symbol($macroname), M, alias)
         end
     end
 end
 
-const FROM_USAGE = "`@from Module use object as alias`"
-const FROM_USAGE_MACRO = "`@from Module use @object as @alias`"
-
-from_error(usage) = throw(ArgumentError("syntax error: expected $usage"))
-
-function _from(condition, usage, _module, object, alias)
-    condition || from_error(usage)
-    isdefined(alias) && throw(ArgumentError("alias `$alias` already defined"))
-
-    names = get_names(_module)
-    import_expr = Expr(:import)
-    for name in names
-        push!(import_expr.args, name)
+macro from(M::Union{Symbol, Expr}, use::Symbol, object::Symbol, as::Symbol, alias::Symbol)
+    if use !== :use && as !== :as
+        throw(ArgumentError("syntax error: expected `@from Module use object as alias`"))
     end
-
-    return quote
-        $import_expr
-        const $alias = $_module.$object
-        nothing
-    end |> esc
+    return create_expression(:import, M, alias, object)
 end
 
-macro from(_module::Union{Symbol, Expr}, use::Symbol, object::Symbol, as::Symbol, alias::Symbol)
-    condition = use == :use && as == :as
-    _from(condition, FROM_USAGE, _module, object, alias)
+macro from(M::Union{Expr, Symbol}, use::Symbol, expr::Expr)
+    # expr is something like :(@f as @F)
+    object = nothing
+    alias = nothing
+    if expr.head === :macrocall && expr.args[1] isa Symbol
+        object = expr.args[1]
+        if expr.args[2] isa LineNumberNode && expr.args[3] === :as &&
+           expr.args[4] isa Expr && expr.args[4].head === :macrocall &&
+           expr.args[4].args[1] isa Symbol
+           alias = expr.args[4].args[1]
+        end
+    end
+    if use !== :use && (object === nothing || alias === nothing)
+        throw(ArgumentError("syntax error: expected `@from Module use @object as @alias`"))
+    end
+    return create_expression(:import, M, alias, object)
 end
 
-macro from(_module::Union{Expr, Symbol}, use::Symbol, object_as_alias::Expr)
-    condition = use == :use && @capture(object_as_alias, @object_ as @alias_)
-    _from(condition, FROM_USAGE_MACRO, _module, object, alias)
-end
-
-macro from(exprs...)
-    from_error("$FROM_USAGE or $FROM_USAGE_MACRO")
+function create_expression(import_or_using::Symbol, M::Union{Symbol, Expr},
+                           alias::Symbol, object=nothing)
+    symbols = get_names(M)
+    import_expr = Expr(import_or_using, Expr(:., symbols...))
+    rhs_expr = Expr(:escape, object === nothing ? last(symbols) : Expr(:., last(symbols), QuoteNode(object)))
+    if import_or_using === :import # hide M behind a gensym baremodule
+        s = gensym()
+        import_expr = Expr(:module, false, Expr(:escape, s), Expr(:block, import_expr))
+        if rhs_expr.args[1] isa Symbol
+            rhs_expr.args[1] = Expr(:., s, QuoteNode(rhs_expr.args[1]))
+        else # esc expr
+            rhs_expr.args[1] = Expr(:., Expr(:., s, QuoteNode(rhs_expr.args[1].args[1])), rhs_expr.args[1].args[2])
+        end
+    end
+    const_expr = Expr(:const, Expr(:global, Expr(:(=), alias, rhs_expr)))
+    return_expr = Expr(:toplevel, import_expr, const_expr, nothing)
+    return return_expr
 end
 
 # utility function to extract names from expression
 function get_names(x)
-    isa(x, Symbol)   && return (x, )
-    x.head == :quote && return (x.args[1], )
-    x.head == :.     && return (get_names(x.args[1])..., get_names(x.args[2])...)
+    isa(x, Symbol)    && return (x, )
+    isa(x, QuoteNode) && return (x.value, )
+    x.head === :quote && return (x.args[1], )
+    x.head === :.     && return (get_names(x.args[1])..., get_names(x.args[2])...)
     throw(ArgumentError("invalid module name"))
 end
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,78 +1,119 @@
-using ImportMacros
-using Base.Test
+using ImportMacros, Test, Pkg
 
-module A
-    export foo
-    foo() = true
-    Foo() = true
-    macro foo(x) x end
-    macro Foo() true end
-    module B
-        export bar
-        bar() = true
-        Bar() = true
-        macro bar(x, y) x, y end
-        module C
-            export baz
-            baz() = true
-            Baz() = true
-            macro baz(x, y, z) x, y, z end
-        end
-    end
-end
+# Add the test-package
+Pkg.develop(PackageSpec(path = joinpath(@__DIR__, "A")))
 
 @testset "@import" begin
-    # A
-    @test (@import A as a) == nothing
-    @import A as a
-    @test_throws UndefVarError foo()
-    @test_throws UndefVarError Foo()
-    @test a.foo()
-    @test a.Foo()
+    # A.B.C
+    @test (@import A.B.C as abc) == nothing
+    @import A.B.C as abc
+    @test_throws UndefVarError baz()
+    @test_throws UndefVarError C.baz()
+    @test_throws UndefVarError Baz()
+    @test_throws UndefVarError C.Baz()
+    @test abc.baz()
+    @test abc.Baz()
 
     # A.B
     @test (@import A.B as ab) == nothing
     @import A.B as ab
     @test_throws UndefVarError bar()
+    @test_throws UndefVarError B.bar()
     @test_throws UndefVarError Bar()
+    @test_throws UndefVarError B.Bar()
     @test ab.bar()
     @test ab.Bar()
 
-    # A.B.C
-    @test (@import A.B.C as abc) == nothing
-    @import A.B.C as abc
-    @test_throws UndefVarError baz()
-    @test_throws UndefVarError Baz()
-    @test abc.baz()
-    @test abc.Baz()
+    # A
+    @test (@import A as a) == nothing
+    @import A as a
+    @test_throws UndefVarError foo()
+    @test_throws UndefVarError A.foo()
+    @test_throws UndefVarError Foo()
+    @test_throws UndefVarError A.Foo()
+    @test a.foo()
+    @test a.Foo()
 end # testset
 
 @testset "@from" begin
-    include("from_tests.jl")
+    # A.B.C
+    @test (@from A.B.C use baz as bz) == nothing
+    @from A.B.C use baz as bz
+    @from A.B.C use Baz as Bz
+    @from A.B.C use @baz as @bz
+    @test_throws UndefVarError baz()
+    @test_throws UndefVarError C.baz()
+    @test_throws UndefVarError Baz()
+    @test_throws UndefVarError C.Baz()
+    @test_throws UndefVarError @eval $(Symbol("@baz"))
+    @test_throws UndefVarError @eval C.$(Symbol("@baz"))
+    @test bz()
+    @test Bz()
+    @test @eval (@bz 1 2 3) == (1, 2, 3)
+
+    # A.B
+    @test (@from A.B use bar as b) == nothing
+    @from A.B use bar as b
+    @from A.B use Bar as Br
+    @from A.B use @bar as @b
+    @test_throws UndefVarError bar()
+    @test_throws UndefVarError B.bar()
+    @test_throws UndefVarError Bar()
+    @test_throws UndefVarError B.Bar()
+    @test_throws UndefVarError @eval $(Symbol("@bar"))
+    @test_throws UndefVarError @eval B.$(Symbol("@bar"))
+    @test b()
+    @test Br()
+    @test @eval (@b 1 2) == (1, 2)
+
+    # A
+    @test (@from A use foo as f) == nothing
+    @from A use foo as f
+    @from A use Foo as F
+    @from A use @foo as @f
+    @from A use @Foo as @F
+    @test_throws UndefVarError foo()
+    @test_throws UndefVarError A.foo()
+    @test_throws UndefVarError Foo()
+    @test_throws UndefVarError A.Foo()
+    @test_throws UndefVarError @eval $(Symbol("@foo"))
+    @test_throws UndefVarError @eval A.$(Symbol("@foo"))
+    @test_throws UndefVarError @eval $(Symbol("@Foo"))
+    @test_throws UndefVarError @eval A.$(Symbol("@Foo"))
+    @test f()
+    @test F()
+    @test @eval (@f 1) == 1
+    @test @eval @F
 end
 
 @testset "@using" begin
-    # A
-    @test (@using A as a) == nothing
-    @using A as a
-    @test foo()
-    @test_throws UndefVarError Foo()
-    @test a.foo()
-    @test a.Foo()
+    # A.B.C
+    @test (@using A.B.C as abc) == nothing
+    @using A.B.C as abc
+    @test baz()
+    @test C.baz()
+    @test_throws UndefVarError Baz()
+    @test C.Baz()
+    @test abc.baz()
+    @test abc.Baz()
 
     # A.B
     @test (@using A.B as ab) == nothing
     @using A.B as ab
     @test bar()
+    @test B.bar()
     @test_throws UndefVarError Bar()
+    @test B.Bar()
     @test ab.bar()
     @test ab.Bar()
 
-    # A.B.C
-    @test (@using A.B.C as abc) == nothing
-    @using A.B.C as abc
-    @test baz()
-    @test_throws UndefVarError Baz()
-    @test abc.baz()
-    @test abc.Baz()
+    # A
+    @test (@using A as a) == nothing
+    @using A as a
+    @test foo()
+    @test A.foo()
+    @test_throws UndefVarError Foo()
+    @test A.Foo()
+    @test a.foo()
+    @test a.Foo()
 end # testset
